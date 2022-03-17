3bf0b4401ccb9a25fb02be51f05b2905cea5c4df
倚窗听雨丿醉无心 <37876877+Zhang-Wei-666@users.noreply.github.com>
2022-03-17 22:48:58 +0800

    提交新的 eslint 规则配置

--- index.js
+++ index.js
@@ -1,3 +1,53 @@
+/* eslint-disable no-console */
+
 module.exports = {
-  extends: '@antfu'
+  extends: '@antfu',
+  overrides: [
+    {
+      files: ['*.vue'],
+      rules: {
+        // 强制使用一致的缩进
+        '@typescript-eslint/indent': 'off',
+        // Script 缩进选项
+        'vue/script-indent': ['error', 2, {
+          baseIndent: 1,
+          switchCase: 1,
+        }],
+        // 元素属性排序
+        'vue/attributes-order': ['error', {
+          order: [
+            // v-if, v-else-if, v-else, v-show, v-cloak
+            'CONDITIONALS',
+            // v-for
+            'LIST_RENDERING',
+            // is, v-is
+            'DEFINITION',
+            // v-once, v-pre
+            'RENDER_MODIFIERS',
+            // id
+            'GLOBAL',
+            // ref, key
+            'UNIQUE',
+            // slot, v-slot
+            'SLOT',
+            // v-model
+            'TWO_WAY_BINDING',
+            // v-custom-directive
+            'OTHER_DIRECTIVES',
+            // custom-prop="foo", v-bind:prop="foo", :prop="foo"
+            'OTHER_ATTR',
+            // v-text, v-html
+            'CONTENT',
+            // @click="functionCall" v-on="event"
+            'EVENTS',
+          ],
+        }],
+      },
+    },
+  ],
+  rules: {
+    // 要求或禁止使用分号代替 ASI
+    'semi': ['error', 'always'],
+    '@typescript-eslint/semi': ['error', 'always'],
+  },
 };
