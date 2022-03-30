a648055a2e2b65ccf97e8511776cab0365a11c1b
倚窗听雨丿醉无心 <994039348@qq.com>
2022-03-30 11:39:22 +0800

    完善注释

--- index.js
+++ index.js
@@ -7,8 +7,10 @@ module.exports = {
       files: ['*.vue'],
       rules: {
         // 强制使用一致的缩进
+        //  - 关闭是因为使用 `vue/script-indent` 配置 `.vue` 文件下的缩进选项
         '@typescript-eslint/indent': 'off',
         // Script 缩进选项
+        //  - 配置缩进为 2 个空格
         'vue/script-indent': ['error', 2, {
           baseIndent: 1,
           switchCase: 1,
@@ -49,6 +51,7 @@ module.exports = {
   ],
   rules: {
     // 要求或禁止使用分号代替 ASI
+    //  - 必须使用分号
     'semi': ['error', 'always'],
     '@typescript-eslint/semi': ['error', 'always'],
   },
